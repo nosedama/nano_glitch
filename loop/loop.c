dbb561f8e1f3f61d50f2df68843a2906adc830be
nosedama <88482930+nosedama@users.noreply.github.com>
2022-07-26 14:40:51 +0200

    [nano] simple loop prog for fault search

--- loop/loop.c
+++ loop/loop.c
@@ -1,20 +1,42 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include "usart.h"
  
-#define BLINK_DELAY_MS 100
+#define BLINK_DELAY_MS (100)
+#define LOOP_ITERATIONS (100)
  
 int main (void)
 {
- /* set pin 5 of PORTB for output*/
- DDRB |= _BV(DDB5);
- 
- while(1) {
-  /* set pin 5 high to turn led on */
-  PORTB |= _BV(PORTB5);
-  _delay_ms(BLINK_DELAY_MS);
- 
-  /* set pin 5 low to turn led off */
-  PORTB &= ~_BV(PORTB5);
-  _delay_ms(BLINK_DELAY_MS);
- }
+	volatile uint32_t i, j, count, repetition;
+
+	/* Set pin 5 of PORTB for output*/
+	DDRB |= _BV(DDB5);
+
+	uart_init(BAUD_CALC(115200));
+	stdout = &uart0_io;
+	stdin = &uart0_io;
+
+	/* Enable interrupts (needed for the uart library) */
+	sei();
+
+	repetition = 0;
+
+	while(1) {
+		/* Set trigger high */
+		PORTB |= _BV(PORTB5);
+
+		count = 0;
+		for(i=0; i<LOOP_ITERATIONS; i++) {
+			for(j=0; j<LOOP_ITERATIONS; j++) {
+				count++;
+			}
+		}
+
+		/* Set trigger low */
+		PORTB &= ~_BV(PORTB5);
+		//_delay_ms(BLINK_DELAY_MS);
+
+		printf("%lu %lu %lu %lu\r\n", i, j, count, repetition++);
+	}
 }
